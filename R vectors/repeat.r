47857b20ddaffe9236a49f739a23b0c8a0b017a6
Arpan-tech-create <bhoiarpan123@gmail.com>
2022-01-27 22:39:27 +0530

    commit on 'master'

--- R vectors/repeat.r
+++ R vectors/repeat.r
@@ -8,5 +8,5 @@ repeat_data
 
 #now,,, 
 
-repeat_data<-rep(c(1,2,3,4),time=4)  # whole element's set is repeated 4 times
+repeat_data<-rep(c(1,2,3,4),times=c(2,1,5,4))  # whole element's set is repeated 4 times
 repeat_data
---
+++ R vectors/sequence_function.r
@@ -0,0 +1,2 @@
+seq1<-seq(from=0,to=100,by=30)
+seq1
